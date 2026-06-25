c3e2daa9516c84aa9afcef4a065a7d17d82777b3
NAZMUL HAKIM <thegadgetghor@gmail.com>
2026-06-26 05:52:48 +0600

    Implement card filtering and wishlist alert
    Added filtering functionality for cards based on category and wishlist alert.

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ document.querySelectorAll(".buy-btn").forEach(button=>{
         document.getElementById("cart-count").innerText = cart;
     });
 });
+const filterButtons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".card");
+
+filterButtons.forEach(button=>{
+button.addEventListener("click",()=>{
+
+const category = button.dataset.category;
+
+cards.forEach(card=>{
+
+if(category==="all" || card.dataset.category===category){
+card.style.display="block";
+}else{
+card.style.display="none";
+}
+
+});
+
+});
+});
+
+document.querySelectorAll(".wishlist-btn").forEach(btn=>{
+
+btn.addEventListener("click",()=>{
+
+alert("❤️ Added to Wishlist");
+
+});
+
+});
