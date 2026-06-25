1bc7d695e84d2364be47db34a89f47133fae4903
thegadgetghor-lang <thegadgetghor@gmail.com>
2026-06-26 05:19:39 +0600

    Add event listener for buy buttons to alert user

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const searchBox = document.getElementById("searchBox");
 searchBox.addEventListener("keyup", function () {
     console.log("Searching:", searchBox.value);
 });
+const buttons = document.querySelectorAll(".buy-btn");
+
+buttons.forEach(btn=>{
+    btn.addEventListener("click",()=>{
+        alert("✅ Product Added To Cart");
+    });
+});
