4f307c94eda9d2c9176c438e4e7062bd9a36b3ee
thegadgetghor-lang <thegadgetghor@gmail.com>
2026-06-26 05:13:58 +0600

    Add search box event listener to script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 alert("Welcome to The Gadget Ghor");
+const searchBox = document.getElementById("searchBox");
+
+searchBox.addEventListener("keyup", function () {
+    console.log("Searching:", searchBox.value);
+});
